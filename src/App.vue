07f0dafb5e609e5bad8a76ee091927b101dbd0d3
Jorge Escamilla <corem96@gmail.com>
2018-06-14 01:23:33 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+
+    <router-view/>
+
+    <movie-list></movie-list>
+
+  </div>
+</template>
+
+<script>
+import movieList from '@/components/sections/movie-list'
+
+export default {
+  name: 'App',
+  components: {
+    'movie-list': movieList
+  }
+}
+</script>
+
+<style lang="scss"></style>
---
+++ src/components/sections/movie-list.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div v-for="movie in movies" :key="movie.id">
+      {{ movie.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => {
+      return {
+        movies: [
+          { id:1 , title: 'Pulp Fiction' },
+          { id:2 , title: 'Blade Runner 2049' },
+          { id:3 , title: 'Dunkirk' },
+          { id:4 , title: 'Baby Driver' },
+        ]
+      }
+    }
+  }
+</script>
