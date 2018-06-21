6af8d2ea4152a01ecd42ded63cecc6621e487a76
Jorge Escamilla <corem96@gmail.com>
2018-06-21 00:33:11 -0600

    carga datos de generos

---
+++ src/components/check-filter.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>filter</div>
+</template>
+
+<script>
+
+</script>
--- src/components/sections/movie-filter.vue
+++ src/components/sections/movie-filter.vue
@@ -1,11 +1,25 @@
 <template>
   <div id="movie-filter">
     <h2>Filter results</h2>
+    <div class="filter-group">
+      <check-filter v-for="genre in genres" :key="genre"></check-filter>
+      <!-- <check-filter v-bind="genres"></check-filter> -->
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
+import checkFilter from '@/components/check-filter';
+import Genres from '@/utils/genres'
 
+  export default {
+    components: {
+      'check-filter': checkFilter
+    },
+    data () {
+      return {
+        genres: Genres
+      }
+    }
   }
 </script>
